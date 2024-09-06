19aae30a34da6386a6243a03abcb19f196e1e196
rlaxogh5079 <rlaxogh507906@gmail.com>
2024-09-06 16:04:14 +0900

    :sparkles: 서버 실행을 위한 main.py 파일 생성 (프레임워크 FastAPI 사용)

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi.responses import JSONResponse
+from routes.user_router import user_router
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+app.include_router(user_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=3000, reload=False)

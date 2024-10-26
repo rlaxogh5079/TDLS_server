241b6d4682d435f74c984293124744ef47a7c69d
rlaxogh5079 <rlaxogh507906@gmail.com>
2024-10-26 23:12:15 +0900

    :zap: CORS원칙

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from models.response import TDLSException, ResponseModel
+from starlette.middleware.cors import CORSMiddleware
 from routes.user_router import user_router
 from fastapi import FastAPI, Request
 import uvicorn
@@ -15,7 +16,14 @@ async def tdls_exception_handler(request: Request, exc: TDLSException):
         message="서버 내부 오류가 발생하였습니다",
         detail=exc.message,
     )
-
+    
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=3000, reload=False)

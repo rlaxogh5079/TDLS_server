3562182ef4e4ebde5bf73bed8d87831b414832f3
rlaxogh5079 <rlaxogh507906@gmail.com>
2024-11-18 22:06:33 +0900

    :recycle: 불필요한 import문 제거

--- models/friend.py
+++ models/friend.py
@@ -1,6 +1,5 @@
 from sqlalchemy import String, ForeignKeyConstraint, Enum as SQLEnum
 from sqlalchemy.orm import Mapped, mapped_column
-from pydantic import BaseModel
 from datetime import datetime
 from typing import Dict, Any
 from models.base import Base
